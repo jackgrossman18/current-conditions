33c18d8ed91ddb459e7cca1fed33b02d34fca1ac
Jack Grossman <jackgrossman@Jacks-MacBook-Pro.local>
2018-06-19 15:38:12 -0400

    Removed event second event listener

--- public/js/map.js
+++ public/js/map.js
@@ -1,6 +1,3 @@
-const button = document.getElementsById('spot')
-
-
 mapboxgl.accessToken =
   "pk.eyJ1IjoiamFja2dyb3NzbWFuIiwiYSI6ImNpbWZqeG1hMjAxcHl2Y202cmhlZGRjYXcifQ.1-so8LElW5dTGT5o941u1w";
 var map = new mapboxgl.Map({
